de25171fa3ea326cc3bbd95c6c362aa1a4a7b26a
thibaut morla <thibaut.morla@gmail.com>
2020-08-12 14:16:34 +0200

    Rails new with frontend and form gems

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+chineseroom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: "610561506511", category: "chinese" }
+japaneseroom = { name: "pizzaroom", address: "8 Boundary St, London E2 7JE", phone_number: "610561506512", category: "japanese" }
+italianroom = { name: "Dishoom", address: "9 Boundary St, London E2 7JE", phone_number: "610561506513", category: "italian" }
+frenchroom = { name: "Dishoom", address: "10 Boundary St, London E2 7JE", phone_number: "610561506514", category: "french" }
+belgiamroom = { name: "Dishoom", address: "11 Boundary St, London E2 7JE", phone_number: "610561506515", category: "belgian" }
+
+[ chineseroom, japaneseroom, italianroom, frenchroom, belgiamroom].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
